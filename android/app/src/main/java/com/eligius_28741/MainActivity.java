5afd8b32a47256b01463ecb0c6d17a07a251e293
Crowdbotics <team@crowdbotics.com>
2021-07-12 04:31:41 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/eligius_28741/MainActivity.java
@@ -0,0 +1,15 @@
+package com.eligius_28741;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "eligius_28741";
+  }
+}
